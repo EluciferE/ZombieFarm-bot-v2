da0bfe5e71c9d30b0c477093696e84a7af86eb89
EluciferE <luciferevgeniy@bk.ru>
2021-03-11 03:03:52 +0700

    Muted browser

--- browser.py
+++ browser.py
@@ -7,6 +7,7 @@ from time import sleep
 def open_browser():
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--mute-audio")
     web = webdriver.Firefox(executable_path="C:\\FirefoxWebDriver\\geckodriver.exe", options=options)
     web.maximize_window()
     return web
