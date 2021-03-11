2cefb9b9c607a2ee03b588700ca542b7afe5c257
EluciferE <luciferevgeniy@bk.ru>
2021-03-11 16:02:24 +0700

    screen moved to class GAME im module.py

--- browser.py
+++ browser.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from PIL import Image
-from time import sleep
 
 
 def open_browser():
@@ -12,9 +10,3 @@ def open_browser():
     web.maximize_window()
     return web
 
-
-def screen(web):
-    web.save_screenshot('.game.png')
-    sleep(3)
-    game = Image.open('.game.png')
-    return game
